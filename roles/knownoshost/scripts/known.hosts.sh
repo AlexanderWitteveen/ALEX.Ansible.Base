da277c5518a54614d1cdbdcbef763bfef5d14316
Alexander Witteveen <alex.witteveen@live.nl>
2026-02-15 13:41:18 +0000

    Build 0.1.23

--- roles/knownoshost/scripts/known.hosts.sh
+++ roles/knownoshost/scripts/known.hosts.sh
@@ -5,8 +5,8 @@ hostname=$2
 localuserssh=$3
 
 cp "$localuserssh/known_hosts" "$localuserssh/known_hosts.bak"
-sed "/$hostname:/d" "$localuserssh/known_hosts.bak" | sed "/$ipaddress:/d" > "$localuserssh/known_hosts"
 
+ssh-keygen -R $hostname -f "$localuserssh/known_hosts" 2>/dev/null
 keystring=$(ssh-keyscan -H -t ecdsa $hostname 2>/dev/null)
 if [[ -z "$keystring" ]]; then
   echo "Warning: Server not online"
@@ -14,6 +14,7 @@ else
   echo $keystring >> "$localuserssh/known_hosts"
 fi
 
+ssh-keygen -R $ipaddress -f "$localuserssh/known_hosts" 2>/dev/null
 keystring=$(ssh-keyscan -H -t ecdsa $ipaddress 2>/dev/null)
 if [[ -z "$keystring" ]]; then
   echo "Error: Server not online"
@@ -22,4 +23,4 @@ else
   echo $keystring >> "$localuserssh/known_hosts"
 fi
 
-
+rm "$localuserssh/known_hosts.old" 2>/dev/null
