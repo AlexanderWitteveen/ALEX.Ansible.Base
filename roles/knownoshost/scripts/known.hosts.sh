ba529cab39a0ee0befbdbb666a910ced7727f7bb
Alexander Witteveen <alex.witteveen@live.nl>
2026-06-21 18:24:25 +0000

    Build 0.1.27

--- roles/knownoshost/scripts/known.hosts.sh
+++ roles/knownoshost/scripts/known.hosts.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
 ipaddress=$1
-hostname=$2
-localuserssh=$3
+localuserssh=$2
 
 cp "$localuserssh/known_hosts" "$localuserssh/known_hosts.bak"
+sed "/$ipaddress:/d" "$localuserssh/known_hosts.bak" > "$localuserssh/known_hosts"
 
-ssh-keygen -R $hostname -f "$localuserssh/known_hosts" 2>/dev/null
-keystring=$(ssh-keyscan -H -t ecdsa $hostname 2>/dev/null)
-if [[ -z "$keystring" ]]; then
-  echo "Warning: Server not online"
-else
-  echo $keystring >> "$localuserssh/known_hosts"
-fi
-
-ssh-keygen -R $ipaddress -f "$localuserssh/known_hosts" 2>/dev/null
-keystring=$(ssh-keyscan -H -t ecdsa $ipaddress 2>/dev/null)
+ssh-keygen -R $ipaddress -f "$localuserssh/known_hosts" 2> /dev/null
+keystring=$( ssh-keyscan -H -t ecdsa $ipaddress 2> /dev/null )
 if [[ -z "$keystring" ]]; then
   echo "Error: Server not online"
   exit 2
 else
+  echo "Changed known hosts for $ipaddress"
   echo $keystring >> "$localuserssh/known_hosts"
 fi
-
-rm "$localuserssh/known_hosts.old" 2>/dev/null
 
 exit 0
--- roles/knownproxmoxhost/scripts/known.hosts.sh
+++ roles/knownproxmoxhost/scripts/known.hosts.sh
@@ -4,13 +4,15 @@ ipaddress=$1
 localuserssh=$2
 
 cp "$localuserssh/known_hosts" "$localuserssh/known_hosts.bak"
-sed "/$ipaddress:/d" "$localuserssh/known_hosts.bak" | > "$localuserssh/known_hosts"
+sed "/$ipaddress:/d" "$localuserssh/known_hosts.bak" > "$localuserssh/known_hosts"
 
+ssh-keygen -R $ipaddress -f "$localuserssh/known_hosts" 2> /dev/null
 keystring=$( ssh-keyscan -H -t ecdsa $ipaddress 2> /dev/null )
-if [[ -z $keystring ]]; then
+if [[ -z "$keystring" ]]; then
   echo "Error: Server not online"
   exit 2
 else
+  echo "Changed known hosts for $ipaddress"
   echo $keystring >> "$localuserssh/known_hosts"
 fi
 
