47940277f27dc483e9277a8870680cbf4ee60b65
Alexander Witteveen <alex.witteveen@live.nl>
2026-06-22 11:47:01 +0000

    Build 0.1.29

--- roles/knownoshost/scripts/known.hosts.sh
+++ roles/knownoshost/scripts/known.hosts.sh
@@ -6,14 +6,22 @@ localuserssh=$2
 cp "$localuserssh/known_hosts" "$localuserssh/known_hosts.bak"
 sed "/$ipaddress:/d" "$localuserssh/known_hosts.bak" > "$localuserssh/known_hosts"
 
-ssh-keygen -R $ipaddress -f "$localuserssh/known_hosts" 2> /dev/null
-keystring=$( ssh-keyscan -H -t ecdsa $ipaddress 2> /dev/null )
-if [[ -z "$keystring" ]]; then
+# ssh-keygen -R $ipaddress -f "$localuserssh/known_hosts" 2> /dev/null
+# keystring=$( ssh-keyscan -H $ipaddress 2> /dev/null )
+# if [[ -z "$keystring" ]]; then
+#   echo "Error: Server not online"
+#   exit 2
+# else
+#   echo "Changed known hosts for $ipaddress"
+#   echo $keystring >> "$localuserssh/known_hosts"
+# fi
+
+ssh-keyscan -H $ipaddress >> "$localuserssh/known_hosts" 2> /dev/null
+if [[ $(cat "$localuserssh/known_hosts" | grep $ipaddress | wc -l) -eq 0 ]]; then
   echo "Error: Server not online"
   exit 2
 else
   echo "Changed known hosts for $ipaddress"
-  echo $keystring >> "$localuserssh/known_hosts"
 fi
 
 exit 0
